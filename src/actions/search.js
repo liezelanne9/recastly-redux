44cdbe9fdf1795f15c0218daf70214561c4c9edf
liezelanne9 <liezelanne9@gmail.com>
2019-03-06 15:51:03 -0800

    completed currentVideo and videoList reducers

--- src/actions/search.js
+++ src/actions/search.js
@@ -4,17 +4,26 @@ import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
-function handleVideoSearch(q) {
-  var options = {
-      key: YOUTUBE_API_KEY,
-      query: q
-    };
-  //TODO:  Write an asynchronous action to handle a video search!
-    return {
-      type: 'HANDLE_VIDEO_SEARCH',
-      q: searchYouTube(this.options, q) // fix me
-    }
 
+function handleVideoSearch(q) {
+  //TODO:  Write an asynchronous action to handle a video search!
+    return () => {
+        searchYouTube({YOUTUBE_API_KEY, q}, () => {
+          changeVideo();
+          changeVideoList();
+        })
+    }
+      // distpatch(changeVideo({data}))
+      // dispatch(changeVideoList({data}))
+      // searchHandler: this.setState q: e.target.value 
+      // searchYouTube(this.options, ) // fix me 
+    
+// ({data}) => {
+//         distpatch(changeVideo({data}))
+//         dispatch(changeVideoList({data}))
+//       }
 };
 
 export default handleVideoSearch;
+
+// call handleVideoSearch
